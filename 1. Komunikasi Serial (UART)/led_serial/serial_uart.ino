2f4b0fd1da70a79f9103feca389a259715d450a7
Ucky Pradestha <uckypradestha@gmail.com>
2026-04-05 20:19:18 +0700

    Change LED pin to custom defined pin

--- 1. Komunikasi Serial (UART)/led_serial/serial_uart.ino
+++ 1. Komunikasi Serial (UART)/led_serial/serial_uart.ino
@@ -1,14 +1,16 @@
+const int ledPin = 8;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledPin, HIGH);
   Serial.println("Hello World");
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(ledPin, LOW);
   delay(1000);
 }
